5e7b4ece3d7f7f9fa9031c6f9a1dc35519b2904e
ohju96 <ohju96@naver.com>
2022-09-20 13:17:42 +0900

    프로젝트 셋팅

---
+++ SpringJpaMongo/src/test/java/com/example/springjpamongo/SpringJpaMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
