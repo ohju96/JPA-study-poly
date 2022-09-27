6cfa0cc21abb04735f1c967335f75699d4bca89a
ohju96 <ohju96@naver.com>
2022-09-27 13:21:50 +0900

    프로젝트 셋팅

---
+++ springSession1/src/test/java/com/example/springsession1/SpringSession1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsession1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSession1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springSession2/src/main/java/com/example/springsession2/SpringSession2Application.java
@@ -0,0 +1,13 @@
+package com.example.springsession2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSession2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSession2Application.class, args);
+    }
+
+}
